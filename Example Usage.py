cafebda81a2ed25f509c37f0e6e103189f70f204
packetvitality <45752781+packetvitality@users.noreply.github.com>
2019-01-07 22:48:38 -0700

    Update Example Usage.py

--- Example Usage.py
+++ Example Usage.py
@@ -2,8 +2,8 @@ from PyFunnels import PyFunnels
 #Specify the output file for each tool in a dictionary.
 source_files = {
     "spiderfoot":"/path/to/file/spiderfoot.db",
-    "recon_ng":"/path/to/file/recon-ng-tester.db",
-    "TheHarvester":"/path/to/file/theharvester-tester.xml",
+    "recon_ng":"/path/to/file/recon-ng.db",
+    "TheHarvester":"/path/to/file/theharvester.xml",
     "photon":"/path/to/file/photon_results/",
     "nmap":"/path/to/file/nmap_results.xml"
 }
